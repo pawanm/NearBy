a7e76de23fd4d920526a1ba11698a65d3d753ecd
PawanM <admin@PawanM.local>
2018-05-25 09:01:26 +0530

    changes in recylcer row

--- NearBy/app/src/main/java/demo/tala/venue/model/VenueModel.java
+++ NearBy/app/src/main/java/demo/tala/venue/model/VenueModel.java
@@ -1,27 +1,41 @@
 package demo.tala.venue.model;
 
+import demo.tala.venue.util.Logger;
+
 public class VenueModel {
 
-  private String name;
-  private String distance;
-  private String venueIconUrl;
+    private String name;
+    private String distance;
+    private String venueIconUrl;
 
-  public VenueModel(String name, String distance, String iconUrl) {
-    this.name = name;
-    this.distance = distance;
-    this.venueIconUrl = iconUrl;
-  }
+    public VenueModel(String name, String distance, String iconUrl) {
+        this.name = name;
+        this.distance = getParsedDistance(distance);
+        this.venueIconUrl = iconUrl;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getDistance() {
-    return distance;
-  }
+    public String getDistance() {
+        return distance;
+    }
 
-  public String getVenueIconUrl() {
-    return venueIconUrl;
-  }
+    public String getVenueIconUrl() {
+        return venueIconUrl;
+    }
+
+    private String getParsedDistance(String distance) {
+        try {
+            int dis = Integer.parseInt(distance)/1000;
+            return String.valueOf(dis);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Logger.logE(ex.getMessage());
+        }
+        return "";
+    }
+
 
 }
